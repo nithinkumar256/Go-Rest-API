b0821c66f0c67e8e922571c521f35c34b1552b43
nithinkumar256 <36795817+nithinkumar256@users.noreply.github.com>
2022-04-18 20:37:03 +0530

    Create data.go

---
+++ data.go
@@ -0,0 +1,16 @@
+package Data
+
+var Months = map[int]string{
+	12: "DEC",
+	11: "NOV",
+	10: "OCT",
+	9:  "SEP",
+	8:  "AUG",
+	7:  "JUL",
+	6:  "JUN",
+	5:  "MAY",
+	4:  "APR",
+	3:  "MAR",
+	2:  "FEB",
+	1:  "JAN",
+}
